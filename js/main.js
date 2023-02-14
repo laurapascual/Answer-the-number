b4a6a3cfdfb563fea8967163d03269f9855d486e
laurapascual <laurapascualmateo@gmail.com>
2023-02-14 08:52:09 +0100

    add variables

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 'use strict';
 
+const btnEl = document.querySelector('.js-btn');
+const choiceEl = document.querySelector('.js-choice');
+const clueEl = document.querySelector('.js-clue');
+const attemptsEl = document.querySelector('.js-attempts');
+
+function getRandomNumber(max) {
+ return Math.ceil(Math.random() * max);
+}
+
+
+
+btnEl.addEventListener('click', handleBtnClick);
