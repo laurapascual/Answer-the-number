23ffe4be7c92509041458c31461886f2b37f2efd
laurapascual <laurapascualmateo@gmail.com>
2023-02-14 09:27:20 +0100

    update js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const choiceEl = document.querySelector('.js-choice');
 const clueEl = document.querySelector('.js-clue');
 const attemptsEl = document.querySelector('.js-attempts');
 const randomNumber = getRandomNumber(100);
+let sumAttempts = 0;
 
 function getRandomNumber(max) {
  return Math.ceil(Math.random() * max);
@@ -15,17 +16,30 @@ function printClue(message) {
     clueEl.value = message;
 }
 
-function handleBtnClick(event) {
-    event.preventDefault();
-    const userChoice = parseInt(choiceEl.value);
+function printAttempts(number) {
+    attemptsEl.value = number;
+}
 
+function attemptsNumber() {
+    sumAttempts++;
+    printAttempts(`Número de intentos: ${sumAttempts}`);
+}
+
+function handleBtnClick(event) {
+    const userChoice = parseInt(choiceEl.value);
+    attemptsNumber();
     if (userChoice > randomNumber && userChoice <= 100) {
         printClue('Demasiado alto');
         
     } else if (userChoice < randomNumber) {
         printClue('Demasiado bajo');
     } else if (userChoice === randomNumber) {
-        printClue ('Has ganado campeona!!!');
+        printClue('Has ganado campeona!!!');
+    } else {
+        if(randomNumber > 1 || randomNumber < 100) {
+            printClue('El número debe estar entre 1 y 100');
+        }
+        
     }
 }
 
